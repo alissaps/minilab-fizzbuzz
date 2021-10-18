3151a13595a952d6d579748eb710ce6bacd71491
Alissa Seixas <alissaps@gmail.com>
2021-10-18 18:51:19 -0300

    finish fizzbuzz

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 let count = 1;
 
 while (count <= 100) {
-  if (count % 5 === 0 && count % 3 === 0) {
+  if (count % 3 === 0 && count % 5 === 0) {
     console.log("fizz buzz");
-  } else if (count % 5 === 0) {
-    console.log("buzz");
   } else if (count % 3 === 0) {
     console.log("fizz");
+  } else if (count % 5 === 0) {
+    console.log("buzz");
   } else {
     console.log(count);
   }
