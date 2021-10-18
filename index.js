f73cf97c983492378065fea1c7ee7c6e25d8c068
Alissa Seixas <alissaps@gmail.com>
2021-10-18 18:45:48 -0300

    finish fizzbuzz

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-// Seu código aqui!
-
 let count = 1;
 
 while (count <= 100) {
-  if (i % 3 === 0) {
+  if (count % 5 === 0 && count % 3 === 0) {
+    console.log("fizz buzz");
+  } else if (count % 5 === 0) {
+    console.log("buzz");
+  } else if (count % 3 === 0) {
     console.log("fizz");
+  } else {
+    console.log(count);
   }
 
   count++;
